1e198b0a244a3fcbf0bb3fe3bad49f3169bb5eb5
sotiriskarageorgopoulos <sotiriskarageorgopoulos@gmail.com>
2023-12-18 20:32:44 +0200

    The project configuration for cloud has been removed...

--- functions/util/config.js
+++ functions/util/config.js
@@ -3,13 +3,13 @@
  */
 
 const config = {
-    apiKey: "AIzaSyCcthMEQ2NLzD424L2R8Q0Gz95BZl2MtnE",
-    authDomain: "smart-hotel-7965b.firebaseapp.com",
-    projectId: "smart-hotel-7965b",
-    storageBucket: "smart-hotel-7965b.appspot.com",
-    messagingSenderId: "844488714171",
-    appId: "1:844488714171:web:6fb4bad7bfac004be7869e",
-    measurementId: "G-XCES5GHSTZ"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 }
 
 module.exports = config
